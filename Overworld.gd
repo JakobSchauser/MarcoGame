a27bc4c67b53c9715378b3d8bf1f7de5a73c5692
JakobSchauser <jakob.schauser@gmail.com>
2022-09-06 17:17:47 +0200

    made overworld work with character select

--- Overworld.gd
+++ Overworld.gd
@@ -8,7 +8,15 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	for i in range(len(G.players_data)):
+		var d = G.players_data[i]
+		var p = player_scene.instance()
+		p.worm_name = d["name"]
+		p.player_num = d["player_num"]
+		p.color = d["color"]
+		p.global_position = $StartPositions.get_child(i).global_position
+		add_child(p)
+		
 
 
 func start_level(level):
